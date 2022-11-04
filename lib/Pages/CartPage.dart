6ad93112c148b7b2ed577789f17ebc8549843f8e
MzeroHour <myintzaw2010@gmail.com>
2022-11-04 16:58:45 +0630

    add cart page

--- lib/Pages/CartPage.dart
+++ lib/Pages/CartPage.dart
@@ -36,6 +36,51 @@ class CartPage extends StatelessWidget {
                   padding: EdgeInsets.symmetric(
                     vertical: 9,
                   ),
+                  child: Container(
+                    width: 380,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 3,
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            "images/pizza.png",
+                            height: 80,
+                            width: 150,
+                          ),
+                        ),
+                        Container(
+                          width: 190,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                "Hot Pizzaaaaaa sssssssss",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
